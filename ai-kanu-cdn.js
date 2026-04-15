eef7559e2d03c68028a01a65bbead520215cd81e
Gerald Carido <geraldcastcarido@gmail.com>
2026-04-15 11:08:53 +0800

    change assistant name

--- ai-kanu-cdn.js
+++ ai-kanu-cdn.js
@@ -10294,7 +10294,7 @@
                                     // Image element removed, using background on container 'd' instead
                                     let f = document.createElement("div");
                                     (f.id = "name-container"),
-                                    (f.textContent = "Kanu"),
+                                    (f.textContent = "Atty. Solomon"),
                                         Object.assign(f.style, {
                                             position: "absolute",
                                             bottom: "0",
